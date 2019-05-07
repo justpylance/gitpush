c0dcfc554b25ce1a237b994df32199d62268fd5c
Gera chikin <justpylance@gmail.com>
2019-05-08 00:26:13 +0300

    BLOG V 0.1

--- FLASK/view/APP/config.py
+++ FLASK/view/APP/config.py
@@ -1,2 +1,5 @@
 class Configuration(object):
 	Debug = True
+	use_reloader=True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password@localhost/test1'
--- FLASK/view/APP/main.py
+++ FLASK/view/APP/main.py
@@ -1,5 +1,9 @@
 from app import app
 import view
+from posts.blueprint import posts
+from app import db
+
+app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == '__main__':
 	app.run()
