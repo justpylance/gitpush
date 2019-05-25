59aeaab25ab8058c11a7368a5d4744f3daea34f4
justpylance <justpylance@gmail.cpm>
2019-05-25 13:13:22 +0300

    shoff upgrade main.py

---
+++ sms_parser/SHOFEROFF/add.py
@@ -0,0 +1,11 @@
+
+
+#### main.py
+import random
+
+car_dict['img'] = 'http://carsdo.ru' + random.choice(soup.find('div',class_='preview_photo_mini').find_all('img')).get('src')
+
+#+++++++++++#
+
+
+
